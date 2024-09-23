b819360a9462b4314eea170cd6bca6137e24b653
carolz <carolinasgarcia97@gmail.com>
2024-09-23 19:57:29 -0300

    ✨início

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Os pais",
+        descricao: "Tamires e Daniel se conheceram através da mãe da Tamires, por volta do ano 2006. Namoraram por x anos! E, então se casaram no dia x. No início do ano de 2020, ambos mudaram para Conceição da Aparecida - MG, onde residem até hoje",
+        imagem:"imagem.jpg"
+
+    },
+    {
+        titulo: "A gestação",
+        descricao: "Tamires e Daniel se conheceram através da mãe da Tamires, por volta do ano 2006. Namoraram por x anos! E, então se casaram no dia x. No início do ano de 2020, ambos mudaram para Conceição da Aparecida - MG, onde residem até hoje",
+        imagem:"imagem.jpg"
+
+    },
+    {
+        titulo: "Chá de Bebê",
+        descricao: "Tamires e Daniel se conheceram através da mãe da Tamires, por volta do ano 2006. Namoraram por x anos! E, então se casaram no dia x. No início do ano de 2020, ambos mudaram para Conceição da Aparecida - MG, onde residem até hoje",
+        imagem:"imagem.jpg"
+
+    },
+]
